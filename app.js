16b74e2c6952c6229096126e2d7284cf2922df89
thiendong.iuh <thiendong.iuh@gmail.com>
2021-06-07 10:26:15 +0100

    add db connect

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const uri = "mongodb+srv://thiendong:123467890Aa@realmcluster.x9iyy.mongodb.net/ideaApp?retryWrites=true&w=majority";
 const express = require('express');
 const http = require("http");
 const app = express();
 
 const server =http.createServer(app);
-const io = require("socket.io").listen(server);
+//const io = require("socket.io").listen(server);
 
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -15,3 +16,19 @@ const cors = require('cors');
 
 const dotenv = require('dotenv');
 dotenv.config();
+
+const PORT = process.env.PORT || 8080;  
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(uri)
+.then(() => console.log('db connected'));
+
+mongoose.connection.on('error', err => {
+    console.log(`DB Error: ${err.message}`);
+});
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+//app.use(expressValidator());
+app.use(cors());
