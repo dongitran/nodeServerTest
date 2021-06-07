fc903b35b08f6ab48b650c291753de10ff475bbf
thiendong.iuh <thiendong.iuh@gmail.com>
2021-06-07 12:50:37 +0100

    change jwt secret string

--- controllers/auth.js
+++ controllers/auth.js
@@ -56,7 +56,7 @@ exports.signin = (req,res) => {
         }
         
         //generate token with user id and secret 
-        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET );
+        const token = jwt.sign({_id: user._id}, 'ideaApp' );
         //persist the token as 't' in cookie with expiry date
         res.cookie("t", token, { expire: new Date() + 9999 });
         //return response with user and token to frontend client
